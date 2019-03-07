3ca216a7bb65465d8006103051372c1fdf669e48
Vaninavizc <vanina.idiart@mmibordeaux.com>
2019-03-07 17:56:11 +0100

    version2

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   resources :authors
   resources :arts
   resources :works
-  root to: 'arts#index'
+  root to: 'home#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end

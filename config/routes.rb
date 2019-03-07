6987ecfdd7c6ba26ab5229eadb897bebdc844692
Vaninavizc <vanina.idiart@mmibordeaux.com>
2019-03-07 10:28:44 +0100

    la page d'index

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
+  resources :authors
   resources :arts
   resources :works
+  root to 'arts#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
